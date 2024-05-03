f1a9d67ba22223e0fa1201ddbe52a04cf4ab3ebc
Raphael Stolt <raphael.stolt@gmail.com>
2024-05-03 13:24:40 +0200

    Expands testing

--- tests/Unit/PackageAnalyserTest.php
+++ tests/Unit/PackageAnalyserTest.php
@@ -1,9 +1,18 @@
 <?php
 
 use App\Domain\PackageAnalyser;
+use App\Enum\ViolationStatus;
 use App\Exceptions\NonExistentPackageDirectory;
 
 test('throws exception for non existent package directory', function () {
     $nonExistentPackageDirectory = '/tmp/non-existent-package-directory';
     new PackageAnalyser($nonExistentPackageDirectory);
 })->throws(NonExistentPackageDirectory::class, sprintf("Provided package directory '%s' does not exist", '/tmp/non-existent-package-directory'));
+
+test('violations have status ViolationStatus::False', function () {
+    $packageAnalyser = new PackageAnalyser('/tmp');
+    $violations = $packageAnalyser->getViolations();
+    foreach ($violations as $violation) {
+        expect($violation['status'])->toBe(ViolationStatus::False);
+    }
+});
