84e7de9cd21c02546219deff4e9483aa8b10ef04
Raphael Stolt <raphael.stolt@gmail.com>
2024-05-08 09:08:04 +0200

    Removes unnecessary commands

--- config/commands.php
+++ config/commands.php
@@ -57,7 +57,6 @@ return [
     'hidden' => [
         NunoMaduro\LaravelConsoleSummary\SummaryCommand::class,
         Symfony\Component\Console\Command\DumpCompletionCommand::class,
-        Symfony\Component\Console\Command\HelpCommand::class,
         Illuminate\Console\Scheduling\ScheduleRunCommand::class,
         Illuminate\Console\Scheduling\ScheduleListCommand::class,
         Illuminate\Console\Scheduling\ScheduleFinishCommand::class,
@@ -77,7 +76,12 @@ return [
     */
 
     'remove' => [
-        // ..
+        LaravelZero\Framework\Commands\TestMakeCommand::class,
+        LaravelZero\Framework\Commands\RenameCommand::class,
+        LaravelZero\Framework\Commands\BuildCommand::class,
+        LaravelZero\Framework\Commands\MakeCommand::class,
+        LaravelZero\Framework\Commands\InstallCommand::class,
+        NunoMaduro\Collision\Adapters\Laravel\Commands\TestCommand::class,
     ],
 
 ];
